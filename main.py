a09127e2c0fa69ff6fddf4d9a2311360fc88dcab
mahmoud abdulhamid <mahmoudabdulhamid22@gmail.com>
2025-10-04 23:00:37 +0300

    some updates

--- main.py
+++ main.py
@@ -1,80 +1,10 @@
-from fastapi import FastAPI,status,Depends
-from openai import OpenAI
-from pydantic import BaseModel,Field,EmailStr,field_validator
-from fastapi.responses import RedirectResponse
-#ex1
+from fastapi import FastAPI
+from models import load_text_model,generate_text
+
 app=FastAPI()
-openai_client=OpenAI(api_key="")
 
-@app.get("/",include_in_schema=False)
-def root_controller():
-    return{"status":"healthy"}
-
-# @app.get("/", include_in_schema=False) 
-# def docs_redirect_controller():
-#     return RedirectResponse(url="/docs", status_code=status.HTTP_303_SEE_OTHER)
-
-@app.get("/chat")
-def chat_controller(prompt:str="Inspire me"):
-    response=openai_client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            {"role":"system","content":"you are a helpful assistant ."},
-            {"role":"user","content":prompt}
-        ],
-    )
-    statement=response.choices[0].message.content
-    return{"statement":statement}
-
-#ex2
-class UserCreate(BaseModel):
-    username:str
-    password:str
-
-    @field_validator("password")
-    def validate_password(cls,value):
-        if len(value) < 8:
-            raise ValueError('Password must be at least 8 characters long')
-        if not any(char.isdigit() for char in value):
-            raise ValueError('Password must contain at least one digit')
-        if not any(char.isupper() for char in value):
-            raise ValueError('Password must contain at least one uppercase letter')
-        return value
-
-@app.post("/users")
-async def create_user_controller(user:UserCreate):
-    return{"name":user.username,"message": "Account successfully created"}
-
-
-
-
-#ex3
-def paginate(skip: int = 0, limit: int = 10): 
-        return {"skip": skip, "limit": limit} 
-# filter and paginate results using pagination params
-@app.get("/messages")
-def list_messages_controller(pagination:dict=Depends(paginate)):
-    return
-
-@app.get("/conversations")
-def list_conversations_controller(pagination: dict = Depends(paginate)): 
-    return
-
-if __name__ == "__main__":
-
-    import torch
-    import transformers
-
-    print("=== Torch & CUDA Info ===")
-    print("Torch version:", torch.__version__)
-    print("CUDA available:", torch.cuda.is_available())
-
-    if torch.cuda.is_available():
-        print("CUDA version:", torch.version.cuda)
-        print("GPU name:", torch.cuda.get_device_name(0))
-
-    print("\n=== Transformers Info ===")
-    print("Transformers version:", transformers.__version__)
-
-
-
+@app.get("/generate/text")
+def serve_langauge_model_controller(prompt:str)->str:
+    pipe=load_text_model()
+    output=generate_text(pipe,prompt)
+    return output
